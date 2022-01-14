c53c5770acca4d1d2abed356a875f3e4f393b9e5
Marcin Tomaszewski <marcin.tomaszewski7@wp.pl>
2022-01-14 09:34:06 +0100

    Add react-router-dom.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import { Link } from "react-router-dom";
 import "./App.css";
 
 function App() {
   return (
     <div>
       <h1>Students App</h1>
+      <Link to="/students">Students</Link>
+      <Link to="/projects">Projects</Link>
     </div>
   );
 }
