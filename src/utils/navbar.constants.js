7babf610b26bcc9c8aa0741d70fc79b9f11fcdd6
Marcin Tomaszewski <marcin.tomaszewski7@wp.pl>
2022-01-15 15:15:02 +0100

    Add a MENU_ITEMS variable.

---
+++ src/utils/navbar.constants.js
@@ -0,0 +1,6 @@
+export const MENU_ITEMS = [
+  { name: "Strona Główna", path: "/" },
+  { name: "Starzyści", path: "/students" },
+  { name: "Umiejętności", path: "/skills" },
+  { name: "Projekty", path: "/projects" },
+];
